bc82fab6600a38e335b4add18d2567903fc1ebf0
Bliish.com <hi@bliish.com>
2026-05-23 14:59:22 +0700

    Improve mobile app scrolling

--- src/shell/layout.tsx
+++ src/shell/layout.tsx
@@ -41,7 +41,7 @@ export function Layout(props: {
         <html lang="en" {...htmlAttributes}>
           <head>
             <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
             <meta name="mobile-web-app-capable" content="yes" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
